faca0e272390ff93ae0ffb42e9ee58ef20a2917c
jolivet <jolivet@db5dbbc3-9ce5-4c2a-b8a4-f9d0e103466c>
2014-11-12 00:07:43 +0000

    owl2sqlite base

---
+++ ontology.sql
@@ -0,0 +1,21 @@
+PRAGMA encoding = "UTF-8";
+CREATE TABLE ontology_authority_person_form (
+  id        TEXT NOT NULL,
+  label     TEXT NOT NULL,
+  comment   TEXT
+);
+CREATE TABLE ontology_rejected_person_form (
+  id        TEXT NOT NULL,
+  label     TEXT NOT NULL,
+  apf_id    TEXT NOT NULL REFERENCES ontology_authority_person_form(id)
+);
+CREATE TABLE ontology_topic (
+  id        TEXT NOT NULL,
+  label     TEXT NOT NULL,
+  parent    TEXT NOT NULL
+);
+CREATE TABLE ontology_contains (
+  article_id      TEXT NOT NULL,
+  indexentry_id   TEXT NOT NULL,
+  indexentry_type TEXT NOT NULL   -- topic or person
+)
